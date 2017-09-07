df0ccbbd9e69fe75e3fb6896bcdbf70c0810ce06
Maxwell <maxwell.me@live.com>
2017-09-07 18:53:22 +0800

    format

--- Mix/Helpers/Regex/Memo.swift
+++ Mix/Helpers/Regex/Memo.swift
@@ -4,70 +4,70 @@
 
 /// Deferred, memoized evaluation.
 internal struct Memo<T> {
-  // MARK: Lifecycle
-
-  /// Constructs a `Memo` which lazily evaluates the argument.
-  init(_ unevaluated:  @escaping () -> T) {
-    self.init(unevaluated: unevaluated)
-  }
-
-  /// Constructs a `Memo` which lazily evaluates the passed function.
-  init(unevaluated: @escaping () -> T) {
-    self.init(state: .unevaluated(unevaluated))
-  }
-
-  /// Constructs a `Memo` wrapping the already-evaluated argument.
-  init(evaluated: T) {
-    self.init(state: .evaluated(evaluated))
-  }
-
-  // MARK: Properties
-
-  /// Returns the value held by the receiver, computing & memoizing it first if necessary.
-  var value: T {
-    return state.value.value()
-  }
-
-  // MARK: Private
-
-  /// Initialize with the passed `state`.
-  private init(state: MemoState<T>) {
-    self.state = MutableBox(state)
-  }
-
-  /// The underlying state.
-  ///
-  /// The `enum` implements the basic semantics (either evaluated or un),
-  /// while the `MutableBox` provides us with reference semantics for the
-  /// memoized result.
-  private let state: MutableBox<MemoState<T>>
+    // MARK: Lifecycle
+    
+    /// Constructs a `Memo` which lazily evaluates the argument.
+    init(_ unevaluated:  @escaping () -> T) {
+        self.init(unevaluated: unevaluated)
+    }
+    
+    /// Constructs a `Memo` which lazily evaluates the passed function.
+    init(unevaluated: @escaping () -> T) {
+        self.init(state: .unevaluated(unevaluated))
+    }
+    
+    /// Constructs a `Memo` wrapping the already-evaluated argument.
+    init(evaluated: T) {
+        self.init(state: .evaluated(evaluated))
+    }
+    
+    // MARK: Properties
+    
+    /// Returns the value held by the receiver, computing & memoizing it first if necessary.
+    var value: T {
+        return state.value.value()
+    }
+    
+    // MARK: Private
+    
+    /// Initialize with the passed `state`.
+    private init(state: MemoState<T>) {
+        self.state = MutableBox(state)
+    }
+    
+    /// The underlying state.
+    ///
+    /// The `enum` implements the basic semantics (either evaluated or un),
+    /// while the `MutableBox` provides us with reference semantics for the
+    /// memoized result.
+    private let state: MutableBox<MemoState<T>>
 }
 
 // MARK: Private
 
 /// Private state for memoization.
 private enum MemoState<T> {
-  case evaluated(T)
-  case unevaluated(() -> T)
-
-  /// Return the value, computing and memoizing it first if necessary.
-  mutating func value() -> T {
-    switch self {
-    case let .evaluated(x):
-      return x
-    case let .unevaluated(f):
-      let value = f()
-      self = .evaluated(value)
-      return value
+    case evaluated(T)
+    case unevaluated(() -> T)
+    
+    /// Return the value, computing and memoizing it first if necessary.
+    mutating func value() -> T {
+        switch self {
+        case let .evaluated(x):
+            return x
+        case let .unevaluated(f):
+            let value = f()
+            self = .evaluated(value)
+            return value
+        }
     }
-  }
 }
 
 /// A mutable reference type boxing a value.
 private final class MutableBox<T> {
-  init(_ value: T) {
-    self.value = value
-  }
-
-  var value: T
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    var value: T
 }
--- Mix/Helpers/Regex/ThreadLocal.swift
+++ Mix/Helpers/Regex/ThreadLocal.swift
@@ -2,18 +2,18 @@ import Foundation
 
 /// Convenience wrapper for generically storing values of type `T` in thread-local storage.
 internal final class ThreadLocal<T> {
-  let key: String
-
-  init(_ key: String) {
-    self.key = key
-  }
-
-  var value: T? {
-    get {
-      return Thread.current.threadDictionary[key] as? T
+    let key: String
+    
+    init(_ key: String) {
+        self.key = key
     }
-    set {
-      Thread.current.threadDictionary[key] = newValue
+    
+    var value: T? {
+        get {
+            return Thread.current.threadDictionary[key] as? T
+        }
+        set {
+            Thread.current.threadDictionary[key] = newValue
+        }
     }
-  }
 }
